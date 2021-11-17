0270149783ff4efe3057e364ff2824d96bc17477
sergTesst <sergiycheck@gmail.com>
2021-11-17 20:21:53 +0200

    save ui state

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducer";
+import { loadState, saveState } from "./localStorage";
+
+const preloadedState = loadState();
 
 export const loggerMiddleware = (storeAPI) => (next) => (action) => {
   console.log("dispatching", action);
@@ -7,24 +10,15 @@ export const loggerMiddleware = (storeAPI) => (next) => (action) => {
   console.log("next state", storeAPI.getState());
   return result;
 };
-
-const preloadedState = {
-  todos: {
-    entities: {
-      myCustomId1: {
-        id: "myCustomId1",
-        text: "initial text 1",
-        completed: false,
-      },
-    },
-  },
-  filters: {
-    filterType: "all",
-  },
-};
-
 const store = createStore(rootReducer, preloadedState, applyMiddleware(loggerMiddleware));
 
-console.log(store.getState());
+store.subscribe(() => {
+  const state = store.getState();
+  saveState({
+    // todos: state.todos,
+    // filters: state.filters,
+    ...state,
+  });
+});
 
 export default store;
